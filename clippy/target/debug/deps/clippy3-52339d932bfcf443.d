ff0e436dfda4ce306dc0eebd1789a127a72683d4
TinySnow <lo.tinysnow.ol@outlook.com>
2022-10-18 09:59:18 +0800

    All done

---
+++ clippy/target/debug/deps/clippy3-52339d932bfcf443.d
@@ -0,0 +1,7 @@
+F:\Projects\Rust\rustlings\exercises\clippy\target\debug\deps\clippy3-52339d932bfcf443.rmeta: clippy3.rs
+
+F:\Projects\Rust\rustlings\exercises\clippy\target\debug\deps\clippy3-52339d932bfcf443.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
